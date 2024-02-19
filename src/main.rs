c15cc136f9c7c3173203c9546baa7fb0a2420fa1
Lean Miguel <theleanmiguel@gmail.com>
2024-02-20 08:51:03 +0900

    add preliminary dockerfile

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,9 @@ const SEED_TABLE_COUNT: i32 = 100;
 async fn main() -> Result<()>{    
     dotenv()?;    
     let db = db::new_db().await.context("could not connect to the database")?;
+
     seed::seed_tables_if_needed(&db, SEED_TABLE_COUNT).await.context("could not seed tables")?;
+
     server::serve(db).await.context("could not start server")?;
 
     Ok(())
--- src/server.rs
+++ src/server.rs
@@ -17,8 +17,10 @@ pub async fn serve(db: PgPool) -> anyhow::Result<()> {
     .nest("/api", restaurant_api_router())
     .with_state(state);  
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:8020").await.context("could not bind to address")?;
-    axum::serve(listener, app).await.context("could not start server")?;
+    let port = std::env::var("PORT").context("PORT is not set")?;
 
+    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{}", port)).await.context("could not bind to address")?;
+    axum::serve(listener, app).await.context("could not start server")?;
+    
     Ok(())
 }
